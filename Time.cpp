ec31aeb872fdcf9c5691078e580b1cf0681bafcd
lienputnina <liene.putnina20@gmail.com>
2024-03-20 14:29:11 +0200

    chore: defined the constructors

--- Time.cpp
+++ Time.cpp
@@ -1,1 +1,15 @@
-// class definitions
+#include "Time.h"
+#include <iostream>
+using namespace std;
+
+Time::Time(int timeSecond, int timeMinute, int timeHour) {
+  int second = timeSecond >= 0 && timeSecond < 60 ? timeSecond : 0;
+  int minute = timeMinute >= 0 && timeMinute < 60 ? timeMinute : 0;
+  int hour = timeHour >= 0 && timeHour < 24 ? timeHour : 0;
+}
+
+Time::Time(const Time &object) {
+  second = object.second;
+  minute = object.minute;
+  hour = object.hour;
+};
--- Time.h
+++ Time.h
@@ -5,4 +5,40 @@
 
 using namespace std;
 
+class Time {
+
+  friend ostream &operator<<(ostream &, const Time &);
+  friend istream &operator>>(istream &, const Time &);
+
+private:
+  int second;
+  int minute;
+  int hour;
+
+public:
+  Time(int second, int minute, int hour);
+
+  // kopijas konstruktors
+  // See, if we need it
+  Time(const Time &object);
+  ~Time();
+
+  const Time &operator=(const Time &);
+
+  Time &operator++();   // Prefix increment
+  Time operator++(int); // Postfix increment
+
+  Time &operator--();   // Prefix increment
+  Time operator--(int); // Postfix decrement
+
+  bool operator>(const Time &);
+  bool operator<(const Time &);
+
+  bool operator>=(const Time &);
+  bool operator<=(const Time &);
+
+  bool operator==(const Time &otherArray) const;
+  bool operator!=(const Time &otherArray) const;
+};
+
 #endif
