1db9168810eac64436c47a489cd36ef6a04c8a0d
lienputnina <liene.putnina20@gmail.com>
2024-03-27 15:09:01 +0200

    refactor: refactored increment/decrement methods to reflect expected output

--- copy.cpp
+++ copy.cpp
@@ -1,7 +1,7 @@
 #include "Time.h"
 using namespace std;
 
-int main() {
+int copyFunction() {
   Time timeObject1(5, 14, 59);
   Time timeObject2(12, 25, 6);
 
@@ -21,7 +21,29 @@ int main() {
   cout << "timeObject1 is " << timeObject1 << endl;
   cout << "timeObject2 is " << timeObject2 << endl;
 
-  // Check the prefix/postfix operators
+  cout << "### Prefix increment test ###" << endl;
+  ++timeObject1;
+  ++timeObject2;
+  cout << "timeObject1 prefix increment: " << timeObject1 << endl;
+  cout << "timeObject2 prefix increment: " << timeObject2 << endl;
+
+  cout << "### Postfix increment test ###" << endl;
+  timeObject1++;
+  timeObject2++;
+  cout << "timeObject1 postfix increment: " << timeObject1 << endl;
+  cout << "timeObject2 postfix increment: " << timeObject2 << endl;
+
+  cout << "### Prefix decrement test ###" << endl;
+  --timeObject1;
+  --timeObject2;
+  cout << "timeObject1 prefix decrement: " << timeObject1 << endl;
+  cout << "timeObject2 prefix decrement: " << timeObject2 << endl;
+
+  cout << "### Postfix decrement test ###" << endl;
+  timeObject1--;
+  timeObject2--;
+  cout << "timeObject1 postfix decrement: " << timeObject1 << endl;
+  cout << "timeObject2 postfix decrement: " << timeObject2 << endl;
 
   cout << "### Comparison test 1: operators < and > ###" << endl;
   if (timeObject1 > timeObject2) {
@@ -40,7 +62,6 @@ int main() {
     cout << timeObject1 << " is smaller than or equal to " << timeObject2
          << endl;
   } else {
-    // add more checks for clarity - is it bigger, is it equal etc.
   }
 
   cout << "### Comparison test 3: operators == and != ###" << endl;
